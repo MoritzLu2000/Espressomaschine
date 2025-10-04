bf3aca16c942e7b431e688469e5447ded7ece56d
MoritzLu2000 <moritz.luithle@gmx.de Primary>
2025-10-04 12:20:08 +0200

    Timer funktioniert

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
+#include <Ticker.h>
 
 // Define the connections pins
 #define CLK 17
@@ -10,66 +11,33 @@ int SensorPin = 21;
 
 float voltage = 0;
 int m = 0;
+int i =0 ;
 TM1637Display display(CLK, DIO);  // Create an instance of the TM1637Display
+Ticker Displaytimer;
+
+
+void onTimer() {
+  uint8_t tens = i / 10;
+  uint8_t ones = i % 10;
+
+  uint8_t prevtens = (i-1) / 10;
+
+  if(prevtens<tens){
+    display.showNumberDec(tens, true, 1, 2);
+    display.showNumberDec(ones, true, 1, 3);
+  }else{
+    display.showNumberDec(ones, true, 1, 3);
+  }
+  i++;
+}
 
 
 void setup() {
   Serial.begin(115200);
-
   display.setBrightness(5);  // Set the display brightness (0-7)
+  Displaytimer.attach(1.0, onTimer);
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  sensorValue = analogRead(13);
-  voltage = sensorValue * (5.0 / 1023.0);
-  int u = 0;
-
-  if (voltage > 1) {
-    //counterStart();
-    Serial.println(voltage);
-
-    for (int i = 0; i < 10; i++) {
-      display.clear();                       // Clear the display
-      display.showNumberDec(i, true, 2, 2);  // __01
-      delay(1000);
-      sensorValue = analogRead(13);
-      voltage = sensorValue * (5.0 / 1023.0);
-      Serial.println(voltage);
-      if (voltage < 1) {
-        u++;
-      }
-      if (u == 2) {
-        i = 10;
-      }
-    }
-
-  } else {
-    Serial.print(voltage);
-  }
-}
-
-
-
-void counterStart() {
-  bool boolean = true;
-  int seconds = 0;
-  int v = 0;
-  while (boolean) {
-    display.clear();                             // Clear the display
-    display.showNumberDec(seconds, true, 2, 2);  // __01
-    for (int u = 0; u < 10; u++) {
-      sensorValue = analogRead(13);
-      voltage = sensorValue * (5.0 / 1023.0);
-      Serial.print(voltage);
-
-      if (voltage != 0) {
-        v++;
-      }
-    }
-    if (v >= 8) {
-      boolean = false;
-    }
-  }
 }
